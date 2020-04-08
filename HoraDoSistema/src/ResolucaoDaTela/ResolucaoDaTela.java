b86051348f8542960faf890057b614a42a6e7eb0
Marcos <mpsdo@yahoo.com.br>
2020-04-08 17:52:52 -0300

    teste03

--- HoraDoSistema/src/ResolucaoDaTela/ResolucaoDaTela.java
+++ HoraDoSistema/src/ResolucaoDaTela/ResolucaoDaTela.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package horadosistema;
+package ResolucaoDaTela;
 
 /**
  *
  * @author marco
  */
 public class ResolucaoDaTela {
-    
+    public static void main(String[] args) {
+        System.out.println("Ola,Mundo2");
+    }
 }
